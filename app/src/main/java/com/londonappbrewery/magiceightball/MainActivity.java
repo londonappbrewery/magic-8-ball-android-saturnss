8f35f9f3897649578adcd975bbc424332da91696
saturnss <aimee.bartlett@gmail.com>
2018-02-20 12:11:14 -0500

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,8 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ballDisplay = findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {
+                                    R.drawable.ball1,
+                                    R.drawable.ball2,
+                                    R.drawable.ball3,
+                                    R.drawable.ball4,
+                                    R.drawable.ball5
+                                };
+        Button myButton;
+        myButton = findViewById(R.id.button2);
     }
 }
